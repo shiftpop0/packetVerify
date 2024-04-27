6020ce58072b15e6173037aba751ffefcfa16703
shiftpop0 <w1095406800@163.com>
2024-04-27 23:49:39 +0800

    add 设备管理信息展示 连接数据平面

--- devices/urls.py
+++ devices/urls.py
@@ -3,4 +3,5 @@ from devices.views import *
 
 urlpatterns = [
     path("showDevice", showDevice),
+    path("getPort", getPort),
 ]
--- devices/views.py
+++ devices/views.py
@@ -7,8 +7,19 @@ import json
 def showDevice(request):
     device_url = 'http://127.0.0.1:8001/device'
     deviceInfo = json.loads(requests.get(device_url+'/deviceInfo').text).get('data')
-    netInfo = json.loads(requests.get(device_url+'/netInfo').text).get('data')
+    # netInfo = json.loads(requests.get(device_url+'/netInfo').text).get('data')
     response = []
-    response.append({'deviceId': 0, 'deviceInfo': deviceInfo, 'netInfo': netInfo})
+    response.append({'deviceId': 0, 'deviceInfo': deviceInfo})
+    # test
+    deviceInfo = {"throughput": 200, "verifySpeed": 200, "avgDelay": 0.05, "verifyMode": True, "tableUsage": 0.25}
+    response.append({'deviceId': 1, 'deviceInfo': deviceInfo})
+    print(response)
+    return JsonResponse(response, safe=False)
+
+def getPort(request):
+    device_url = 'http://127.0.0.1:8001/device'
+    devicePort = json.loads(requests.get(device_url+'/portInfo').text).get('data')
+    response = []
+    response= {'deviceId': 0, 'devicePort': devicePort}
     print(response)
     return JsonResponse(response, safe=False)
